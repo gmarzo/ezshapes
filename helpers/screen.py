953a33d66a541018e019b02d986013805e8ec0c3
gmarzo <garrett.marzo@gmail.com>
2024-10-03 17:36:36 -0700

    Finished docstrings for current functions

--- helpers/screen.py
+++ helpers/screen.py
@@ -3,17 +3,42 @@ import pygame
 pygame.init()
 
 class Screen:
-  def __init__(self, height, width, name="Flying A UFO!"):
+  """
+  A wrapper class for the pygame display.
+
+  Attributes:
+    - height (int): height of the screen in pixels.
+    - width (int): width of the screen in pixels.
+    - display (Surface): Created surface from pygame display.
+  """
+  def __init__(self, height:int, width:int, name:str)->None:
+    """
+    Initializes a Screen object.
+
+    Parameters:
+      - height (int): height of the screen in pixels
+      - width (int): width of the screen in pixels
+      - name (str): caption at the top of the screen
+    """
     self.height = height
     self.width = width
     self.display = pygame.display.set_mode((self.width, self.height))
     pygame.display.set_caption(name)
   
-  def __get_screen__(self):
+  def __get_screen__(self)->pygame.Surface:
+    """
+    Returns the Surface object.
+    """
     return self.display
 
-  def get_width(self):
+  def get_width(self)->int:
+    """
+    Returns the width of the screen.
+    """
     return self.width
 
-  def get_height(self):
+  def get_height(self)->int:
+    """
+    Returns the height of the screen.
+    """
     return self.height
